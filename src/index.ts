35c6c8b7bc3ce704b1fb7e940d509bd3df67a632
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-09-13 12:06:53 +0300

    update workflow

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
 export { accountDataQuery, getAccountNonce, getAccountBalance } from './global_state'
 export { default as crypto } from './crypto'
-export {
-  generatePrivateKey,
-  generatePublicKey,
-  derivePrivateKey,
-  derivePublicKey,
-  signMessage,
-  verifyMessage,
-  signTransaction,
-  toHexString,
-  fromHexString,
-} from './crypto'
-export {
-  globalStateClient,
-  txChannel,
-  createGlobalStateChannel,
-  createTransactionChannel,
-  createChannels,
-} from './channels'
+export { generatePrivateKey, generatePublicKey, derivePrivateKey, derivePublicKey, signMessage, verifyMessage, signTransaction, toHexString, fromHexString } from './crypto'
+export { globalStateClient, txChannel, createGlobalStateChannel, createTransactionChannel, createChannels } from './channels'
 export { submitTransaction } from './tx'
 export * from './proto/dist/google/protobuf'
 export * from './proto/dist/google/rpc'
